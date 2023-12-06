ff66f8fe5f363b5477c687bf81627c70fe22b223
DouglassY <teryakiboyzv2@gmail.com>
2023-12-06 19:24:23 +0300

    Robofriends

---
+++ Robofriends/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      scale: {
+        101: "1.01",
+      },
+    },
+  },
+  plugins: [],
+};
